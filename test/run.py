935a8b557726087d6a0bed770e123ea00d2018bb
rishabh06sharma <rs278@buffalo.edu>
2021-06-02 13:33:28 -0400

    deploy on server

--- test/run.py
+++ test/run.py
@@ -1,3 +1,3 @@
 import requests
-re=requests.post("http://127.0.0.1:5000/predict_class",files={"file":open("test_images/sample__4.png","rb")})
+re=requests.post("https://fmnist-app.herokuapp.com/predict_class",files={"file":open("test_images/sample__7.png","rb")})
 print(re.text) ## will return from the predict_class function from main.py
