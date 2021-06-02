4ea75d8f6e5ee433aeb09813164e20f098543062
rishabh06sharma <rs278@buffalo.edu>
2021-06-02 17:53:06 -0400

    optional input image

--- test/run.py
+++ test/run.py
@@ -1,3 +1,12 @@
 import requests
-re=requests.post("https://fmnist-app.herokuapp.com/predict_class",files={"file":open("test_images/sample__7.png","rb")})
+
+print("New Image? (y/n)")
+check_input=input()
+if check_input=="y":
+    directory=input()
+else:
+    print("Using default directory (test image): test_images/sample__1.png")
+    directory=("test_images/sample__1.png")
+
+re=requests.post("https://fmnist-app.herokuapp.com/predict_class",files={"file":open(directory,"rb")})
 print(re.text) ## will return from the predict_class function from main.py
